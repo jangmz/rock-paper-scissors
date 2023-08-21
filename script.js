102e50dfaddf2b8a1d867e869092a67da1200705
jangmz <131482887+jangmz@users.noreply.github.com>
2023-08-21 11:49:20 +0200

    functions for player input, computer input, one round play

--- script.js
+++ script.js
@@ -13,4 +13,52 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+function getPlayerChoice() {
+    let playerInput;
+    while (true) {
+        playerInput = prompt("Choose rock/paper/scissors");
+        playerInput = playerInput.toLowerCase();
+        if (playerInput === "rock" || playerInput === "paper" || playerInput === "scissors") {
+            break;
+        } else {
+            alert("Wrong input! Click OK to try again");
+        }
+    }
+    return playerInput;
+}
+
+function playRound(playerSelection, computerSelection) {
+    switch(playerSelection) {
+        case "rock":
+            if (computerSelection === "scissors") {
+                return "You won!";
+            } else if (computerSelection === "rock") {
+                return "Tie!";
+            } else {
+                return "You lost!";
+            }
+        case "paper":
+            if (computerSelection === "scissors") {
+                return "You lost!";
+            } else if (computerSelection === "rock") {
+                return "You won!";
+            } else {
+                return "Tie!";
+            }
+        case "scissors":
+            if (computerSelection === "scissors") {
+                return "Tie!";
+            } else if (computerSelection === "rock") {
+                return "You lost!";
+            } else {
+                return "You won!";
+            }
+    }
+}
+
+const player = getPlayerChoice();
+const computer = getComputerChoice();
+
+console.log("Player choice:", player);
+console.log("Computer choice:", computer);
+console.log(playRound(player, computer));
